17d3fbfc6d4287d70408b83297b1bb6e66ad1fca
AsyaSkoraya <153989033+AsyaSkoraya@users.noreply.github.com>
2024-03-19 23:30:48 +0500

    task2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        boolean comporator = bean==bean2;
+        System.out.println(comporator);
+
+                Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getCatMssage());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean2.getCatMssage());
+
+
+        boolean comporator2 = catBean1==catBean2;
+        System.out.println(comporator2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String catMssage;
+
+    public String getCatMssage() {
+        return catMssage;
+    }
+
+    public void setCatMssage(String catMssage) {
+        this.catMssage = catMssage;
+    }
+}
